728e7dde300ff57d45cf52dfbc01bc0c9c60d3f1
ming <3268693203@qq.com>
2021-08-04 20:23:23 +0800

    服务消费者搭建完成,并测试成功

--- eureka-parent/service-consumer/src/main/java/org/lq/service/impl/OrderServiceImpl.java
+++ eureka-parent/service-consumer/src/main/java/org/lq/service/impl/OrderServiceImpl.java
@@ -6,7 +6,12 @@ import org.lq.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -15,6 +20,7 @@ import java.util.List;
 /*
     订单服务
  */
+@Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private RestTemplate restTemplate;
@@ -31,11 +37,18 @@ public class OrderServiceImpl implements OrderService {
         if(CollectionUtils.isEmpty(serviceIds))
             return null;
         //根据服务名称获取服务
-        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("service-provide");
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("service-provider ");
         if(CollectionUtils.isEmpty(serviceInstances))
             return null;
         ServiceInstance si = serviceInstances.get(0);
         sb = new StringBuffer();
-        return new ArrayList<Product>();
+        sb.append("http://" + si.getHost() + ":" + si.getPort() + "/product/list");
+        //ResponseEntity:封装了返回数据
+        ResponseEntity<List<Product>> response = restTemplate.exchange(
+                sb.toString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Product>>() {});
+        return response.getBody();
     }
 }
