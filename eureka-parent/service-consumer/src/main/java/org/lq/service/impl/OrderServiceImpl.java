cd27c9eed4f85731bdae13f3cf86d21c8dae5e12
ming <3268693203@qq.com>
2021-08-04 20:43:23 +0800

    yLoaderBalancerClient负载均衡

--- eureka-parent/service-consumer/src/main/java/org/lq/service/impl/OrderServiceImpl.java
+++ eureka-parent/service-consumer/src/main/java/org/lq/service/impl/OrderServiceImpl.java
@@ -6,6 +6,8 @@ import org.lq.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
     private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
-    @Override
+    private LoadBalancerClient loadBalancerClient; //Ribbon 负载均衡器
     public Order selectOrderById(Integer id) {
         return new Order(id,"order-001","中国",319949D,selectProductListByDiscoveryClient());
     }
@@ -51,4 +53,22 @@ public class OrderServiceImpl implements OrderService {
                 new ParameterizedTypeReference<List<Product>>() {});
         return response.getBody();
     }
+    //通过loaderbalance负载均衡器调用
+    private List<Product> selectProductListByLoaderBalancerClient(){
+        StringBuffer sb = null;
+        //根据服务名称获取服务
+        ServiceInstance si = loadBalancerClient.choose("service-provider");
+        if(si == null){
+            return  null;
+        }
+        sb = new StringBuffer();
+        sb.append("http://" + si.getHost() + ":" + si.getPort()+"/product/list");
+        ResponseEntity<List<Product>> response = restTemplate.exchange(
+                sb.toString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Product>>() {});
+        return  response.getBody();
+    }
+
 }
